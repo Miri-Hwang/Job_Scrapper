c6d9d2c3a06656913d5464ba1c98568873031181
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-01-28 17:09:34 +0900

    feat : Import Requests

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+
+
+indeed_result = requests.get(
+    "https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
+
+print(indeed_result.text)
