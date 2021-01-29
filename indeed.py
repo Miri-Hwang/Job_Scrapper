497d44216cdb18a45439a07f5a4713a7a9a2a845
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-01-29 13:18:05 +0900

    feat : Request Each Indeed Page

---
+++ indeed.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 10
+URL = f"https://www.indeed.com/jobs?q=python&start={LIMIT}"
+
+
+def extract_indeed_pages():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("ul", {"class": "pagination-list"})
+    links = pagination.find_all('span')
+
+    pages = []
+
+    for link in links[2:-2]:
+        pages.append(int(link.string))
+
+    max_page = pages[-1]
+    return max_page
+
+
+def extract_indeed_jobs(last_page):
+    for page in range(last_page):
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        print(result.status_code)
--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
 
-indeed_result = requests.get(
-    "https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
+last_indeed_page = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-
-pagination = indeed_soup.find("ul", {"class": "pagination-list"})
-
-links = pagination.find_all('span')
-
-pages = []
-
-for link in links[:-2]:
-    pages.append(int(link.string))
-
-
-max_page = pages[-1]
-print(max_page)
+extract_indeed_jobs(last_indeed_page)
