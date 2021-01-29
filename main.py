9fb150c547f89724bcc9fc0bd405f68b9dc2fb40
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-01-29 11:04:05 +0900

    feat : Extract Page Numbers from Indeed

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 indeed_result = requests.get(
     "https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
 
-print(indeed_result.text)
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("ul", {"class": "pagination-list"})
+
+pages = pagination.find_all('span')
+
+spans = []
+
+for page in pages:
+    spans.append(page)
+
+spans = spans[:-2]
